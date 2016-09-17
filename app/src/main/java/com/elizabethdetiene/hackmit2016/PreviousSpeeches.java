c4f1c47357bcc4cc17175f293d1a897ff1b119a6
Elizabeth DeTienne <elizdet@mit.edu>
2016-09-17 17:08:29 -0400

    some buttons added

---
+++ app/src/main/java/com/elizabethdetiene/hackmit2016/PreviousSpeeches.java
@@ -0,0 +1,13 @@
+package com.elizabethdetiene.hackmit2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreviousSpeeches extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previous_speeches);
+    }
+}
